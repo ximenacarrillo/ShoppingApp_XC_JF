8f35312dee072cc2c49536dac111c1a6325697b3
Jair Fonseca/ <jairfonseca64@yahoo.es>
2022-02-19 10:22:45 -0500

    Base models ended

--- Isi.ShoppingApp.Core/Entities/Cart_Products.cs
+++ Isi.ShoppingApp.Core/Entities/Cart_Products.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Isi.ShoppingApp.Core.Entities
 {
-    class Cart_Products
+    public class Cart_Products
     {
+        public long FK_IdCart { get; }
+        public long FK_IdProduct { get; }
+        public int Quantity
+        {
+            get => quantity;
+            set
+            {
+                if (value > 0)
+                    quantity = value;
+            }
+        }
+        public decimal Price 
+        { 
+            get => price; 
+            set
+            {
+                if (value > 0)
+                    price = value;
+            }
+        }
+        public decimal Discount
+        {
+            get => discount;
+            set
+            {
+                if (value > 0)
+                    discount = value;
+            }
+        }
+
+        public decimal Subtotal
+        {
+            get => (Price - Discount) * Quantity;
+        }
+
+        private int quantity;
+        private decimal price;
+        private decimal discount;
+        public Cart_Products()
+        {
+
+        }
     }
 }
